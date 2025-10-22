7d44545f6f5715f9a2898f20d5077c93a78cbda2
Reema_abde <Reemabdella702@gmail.com>
2025-10-22 22:25:00 +0300

    Fixed deploument issues

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api" // your backend running locally
-    : "https://note-ado-10.onrender.com/api"; // your Render backend URL
+    : "https://note-ado-13.onrender.com/api"; // your Render backend URL
 
 const api = axios.create({
   baseURL: BASE_URL,
